be14c419fa4ac6821ce833279d0c0f8f1c15e9d9
Lucas L <lli60@stevens.edu>
2021-10-21 02:33:55 -0400

    1st commit

---
+++ MovieShop/ApplicationCore/Models/MovieDetailsResponseModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Models
+{
+    public class MovieDetailsResponseModels
+    {
+        //This is called ViewModel, which includes:
+        //properties of movie
+        //list<cast>
+        //list<genres>
+    }
+}
---
+++ MovieShop/MovieShopMVC/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieShopMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.Models;
+using Infrastructure.Services;
+using ApplicationCore.ServiceInterfaces;
+
+namespace MovieShopMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IMovieService _movieService;
+        public HomeController(IMovieService movieService) 
+        {
+            _movieService = movieService;
+        }
+
+        //Routing
+        //https://localhost/home/index
+        //by default it's get
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //Views Folder => Home => Index.cshtml
+            //call movie service class to get a list of movies card models
+            var movieCards = _movieService.GetTop30RevenueMovies();
+            //passing data from controller to view, strongly typed models(most used)
+            //other ways: ViewBag and ViewData
+            ViewBag.PageTitle = "Top Revenue Movies";
+            ViewData["xyz"] = "test data";
+            return View(movieCards);
+        }
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult TopMovies()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
